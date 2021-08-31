f8460a6910a52a43b87c7010672ae7a04152ebb5
YalmiValentina <yalmivale@gmail.com>
2021-08-31 13:17:25 -0400

    entrega el nuevo numero y la suma obtenida

--- trecebasicos/trece.rb
+++ trecebasicos/trece.rb
@@ -3,3 +3,7 @@
 
 #Mostrar números impares entre 1 y 255
 (1..255).each { |num| puts num unless num.even? }
+
+#Muestre la suma
+suma = 0
+(1..255).each { |num| puts "Nuevo número: " + num.to_s + " Suma: " + (suma+=num).to_s}
