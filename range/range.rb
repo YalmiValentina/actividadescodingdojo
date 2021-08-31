d1fc3e70c55ae6c50e9cbd598f13d15adcdf9db9
YalmiValentina <yalmivale@gmail.com>
2021-08-31 13:03:37 -0400

    el ultimo objeto del arreglo es 5

--- range/range.rb
+++ range/range.rb
@@ -1,4 +1,7 @@
-#.include?(value) => devuelve true or false
-
 arreglo = (1..5)
+
+#.include?(value) => devuelve true or false
 puts arreglo.include?(4)
+
+#.last => devuelve el último objeto en el Range
+puts arreglo.last
